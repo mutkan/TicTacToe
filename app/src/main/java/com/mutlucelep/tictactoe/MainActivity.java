1d4e923a18ddacd8ffbaf1c79382b3c75597afab
mutlu <mutkanx@gmail.com>
2018-12-25 03:58:49 -0600

    first initial empty activity

---
+++ app/src/main/java/com/mutlucelep/tictactoe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mutlucelep.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
