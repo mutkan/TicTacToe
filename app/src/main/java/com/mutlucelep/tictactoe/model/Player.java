c3b645d9fec1e9482e96e6f39b69adcd64fde772
mutlu <mutkanx@gmail.com>
2018-12-26 01:01:50 -0600

    Add player model

---
+++ app/src/main/java/com/mutlucelep/tictactoe/model/Player.java
@@ -0,0 +1,11 @@
+package com.mutlucelep.tictactoe.model;
+
+public class Player {
+    public String mName;
+    public String mValue;
+
+    public Player(String name, String value){
+        mName = name;
+        mValue = value;
+    }
+}
