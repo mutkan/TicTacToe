4c1827476a264eab1f63978649df7276b1110d84
mutlu <mutkanx@gmail.com>
2019-01-07 17:23:08 -0600

    Reformat

--- app/src/main/java/com/mutlucelep/tictactoe/viewmodel/GameViewModel.java
+++ app/src/main/java/com/mutlucelep/tictactoe/viewmodel/GameViewModel.java
@@ -11,26 +11,24 @@ import com.mutlucelep.tictactoe.model.Player;
 import static com.mutlucelep.tictactoe.utility.StringUtility.stringFromNumbers;
 
 public class GameViewModel extends ViewModel {
-    public ObservableArrayMap<String, String> mCellsObservableArrayMap;
-    private Game mGame;
+  public ObservableArrayMap<String, String> mCellsObservableArrayMap;
+  private Game mGame;
+Ö
+  public void init(String player1, String player2) {
+    mGame = new Game(player1, player2);
+    mCellsObservableArrayMap = new ObservableArrayMap<>();
+  }
 
-    public void init(String player1, String player2){
-        mGame = new Game(player1,player2);
-        mCellsObservableArrayMap = new ObservableArrayMap<>();
+  public void onClickedCellAt(int row, int column) {
+    if (mGame.mCells != null && mGame.mCells[row][column] == null) {
+      mGame.mCells[row][column] = new Cell(mGame.mCurrentPlayer);
+      mCellsObservableArrayMap.put(stringFromNumbers(row, column), mGame.mCurrentPlayer.mValue);
+      if (mGame.hasGameEnded()) mGame.reset();
+      else mGame.switchPlayer();
     }
+  }
 
-    public void onClickedCellAt(int row, int column){
-        if(mGame.mCells[row][column] == null){
-            mGame.mCells[row][column] = new Cell(mGame.mCurrentPlayer);
-            mCellsObservableArrayMap.put(stringFromNumbers(row,column),mGame.mCurrentPlayer.mValue);
-            if(mGame.hasGameEnded())
-                mGame.reset();
-            else
-                mGame.switchPlayer();
-        }
-    }
-
-    public LiveData<Player> getWinner(){
-        return mGame.mWinner;
-    }
+  public LiveData<Player> getWinner() {
+    return mGame.mWinner;
+  }
 }
